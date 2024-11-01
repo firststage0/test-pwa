45104b56090b7e1a3eb1da132b75880eed632522
firststage <red_tk@mail.ru>
2024-11-01 13:36:40 +0300

    добавлен capacitor для сборки приложения

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,28 @@ import { createPinia } from 'pinia'
 import router from '@/router/index.ts'
 import App from './App.vue'
 import '@/registerServiceWorker'
-// if (navigator.serviceWorker) {
-//   navigator.serviceWorker
-//     .register('/service-worker.js')
-//     .then(registration => {
-//       console.log('Service Worker registered with scope:', registration.scope)
-//     })
-//     .catch(error => {
-//       console.error('Service Worker registration failed:', error)
-//     })
 
-//   }
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register(`service-worker.js`)
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope)
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error)
+    })
+}
 
-Notification.requestPermission().then(permission => {
-  if (permission === 'granted') {
-    console.log('Разрешение на уведомления получено')
-  }
-})
+if ('Notification' in window) {
+  Notification.requestPermission().then(permission => {
+    if (permission === 'granted') {
+      console.log('Разрешение на уведомления получено')
+    }
+  })
+} else {
+  console.log('API Notification не поддерживается в этом окружении')
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
